ad909f203e1d8ee9d7d9d853516c6056a2796f15
Nasser Amjadian <amjadian.n@gmail.com>
2021-04-12 22:21:22 +0430

    add engine map and process map

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,39 @@
-debugger;
 
 list ;
 bundle;
 var bundleFa = bundle.fa.keywords;
 var processMap = new Map();
 var userMap = new Map();
+var engineMap = new Map();
+var tekrriha = [];
+todo()
+
 function todo(){
     console.time("todo");
     for(let i=0; i<list.length ; i++){
         let item = list[i]; 
-        if(processMap.get(item.process_code)){
-            
-
+        item.bundleFa = bundleFa[item.process_code]
+        if(processMap.has(item.process_code)){
+            let valuesP = processMap.get(item.process_code)
+            valuesP.push(item)
+            processMap.set(item.process_code ,valuesP);
         }else{            
-            processMap.set(item.process_code ,item);
-            item.bundleFa = bundleFa[item.process_code]
+            processMap.set(item.process_code ,[item]);
         }
         // bundleFa[item.process_code] && processMap.get(item.process_code) ? processListFa.push(bundleFa[item.process_code]): null;
-        debugger;
+
+
+        if(engineMap.has(item.engine_name)){
+            let valuesE = engineMap.get(item.engine_name)
+            valuesE.push(item)
+            engineMap.set(item.engine_name ,valuesE);
+        }else{            
+            engineMap.set(item.engine_name ,[item]);
+        }
+
     }
     console.timeEnd("todo");
-}
+};
+
+
 
